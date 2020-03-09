e48eff3d145e33e6b249df74bfed3ac8971c13d3
Ciara Hynes R00146580 <ciara.hynes@mycit.ie>
2020-03-09 10:50:10 +0000

    Web scraping with Nokogiri started

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -3,4 +3,30 @@ class CompanyController < ApplicationController
     company = Company.find_by(id: params[:id])
     render json: company
   end
+
+  def scrape
+    require 'nokogiri'
+    require 'open-uri'
+    require 'headless'
+
+    headless = Headless.new
+    headless.start
+
+    browser = Watir::Browser.start(main_page_url)
+    if browser.link(text: "Click here").present?
+      browser.link(text: "Click here").click
+    end
+
+    web = Nokogiri::HTML(open('https://www.google.com/search?q=' + params[:company] + '+subsidiaries'))
+
+    companies = []
+    byebug
+    web.xpath('/html/body/div/div[3]/div/div[2]/div/div/div/div/a').each do |a_tag|
+      companies.append(a_tag.xpath('div/div/div[2]/div/div')[0].content)
+    end
+
+    byebug
+
+    render plain: "Hello"
+  end
 end
--- app/controllers/hello_controller.rb
+++
@@ -1,5 +0,0 @@
-class HelloController < ApplicationController
-  def show
-    render plain: "my name is Ciara"
-  end
-end
